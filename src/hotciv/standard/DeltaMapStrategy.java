00899832bd6b8edfb8262ccda01c6b7bf148a466
Rahbekchyd <mathiasrahbek@hotmail.dk>
2015-11-26 14:55:09 +0100

    Deltamap changes

--- src/hotciv/standard/DeltaMapStrategy.java
+++ src/hotciv/standard/DeltaMapStrategy.java
@@ -6,6 +6,7 @@ import hotciv.framework.GameConstants;
 import hotciv.framework.MapStrategy;
 import hotciv.framework.Player;
 import hotciv.framework.Position;
+import hotciv.framework.Tile;
 import hotciv.framework.Unit;
 
 public class DeltaMapStrategy implements MapStrategy {
@@ -13,11 +14,9 @@ public class DeltaMapStrategy implements MapStrategy {
 	String tileLine;
 	String cityLine;
 	String unitLine;
-	private Game game;
-
+	
 	public void mapLayout(Game game) {
-		this.game = game;
-
+		
 		String[] tileLayout = new String[] {
 				"oooppMoooooooooo",
 				"oophhppppfffppoo",
@@ -74,30 +73,53 @@ public class DeltaMapStrategy implements MapStrategy {
 				"oooooooobloooorsoo",
 				"oooooooooooooooo"
 		};
-
-		generateMap(tileLayout, cityLayout, unitLayout);
+		generateTiles(tileLayout);
+		generateUnit(unitLayout);
+		generateCity(cityLayout);
 	}
 
-	public void generateMap(String[] tileLayout, String[] cityLayout, String[] unitLayout) {
+	public Tile[][] generateTiles(String[] tileLayout) {
+		Tile[][] tiles = new Tile[GameConstants.WORLDSIZE][GameConstants.WORLDSIZE];
 		for ( int i = 0; i < GameConstants.WORLDSIZE; i++ ) {
 			tileLine = tileLayout[i];
+
+			for ( int j = 0; j < GameConstants.WORLDSIZE; j++ ) {
+				char tileChar = tileLine.charAt(j);
+
+				if ( tileChar == 'o' ) { tiles[i][j] = new TileImpl(GameConstants.OCEANS); }
+				if ( tileChar == 'p' ) { tiles[i][j] = new TileImpl(GameConstants.PLAINS); }
+				if ( tileChar == 'M' ) { tiles[i][j] = new TileImpl(GameConstants.MOUNTAINS); }
+				if ( tileChar == 'f' ) { tiles[i][j] = new TileImpl(GameConstants.FOREST); }
+				if ( tileChar == 'h' ) { tiles[i][j] = new TileImpl(GameConstants.HILLS); }			 
+			}
+		}
+		return tiles;
+	}
+
+	public City[][] generateCity(String[] cityLayout) {
+		City[][] cities = new City[GameConstants.WORLDSIZE][GameConstants.WORLDSIZE];
+		for ( int i = 0; i < GameConstants.WORLDSIZE; i++ ) {
 			cityLine = cityLayout[i];
+
+			for ( int j = 0; j < GameConstants.WORLDSIZE; j++ ) {
+				char tileChar = tileLine.charAt(j);
+
+				if ( tileChar == 'r' ) { cities[i][j] = new CityImpl(Player.RED); }
+				if ( tileChar == 'b' ) { cities[i][j] = new CityImpl(Player.BLUE); }	 
+			}
+		}
+		return cities;
+	}
+
+	public Unit[][] generateUnit(String[] unitLayout) {
+		Unit[][] units = new Unit[GameConstants.WORLDSIZE][GameConstants.WORLDSIZE];
+		for ( int i = 0; i < GameConstants.WORLDSIZE; i++ ) {
 			unitLine = unitLayout[i];
 			int k = 0;
 			int extra = (unitLine.length() - GameConstants.WORLDSIZE);
 
 			for ( int j = 0; j < GameConstants.WORLDSIZE; j++ ) {
 				char tileChar = tileLine.charAt(j);
-				char cityChar = cityLine.charAt(j);
-
-				if ( cityChar == 'r' ) { game.buildCity(new Position(i, j), Player.RED); }
-				if ( cityChar == 'b' ) { game.buildCity(new Position(i, j), Player.BLUE); } 
-
-                if ( tileChar == 'o' ) { game.addTile(new Position(i, j), GameConstants.OCEANS); }
-                if ( tileChar == 'p' ) { game.addTile(new Position(i, j), GameConstants.PLAINS); }
-                if ( tileChar == 'M' ) { game.addTile(new Position(i, j), GameConstants.MOUNTAINS); }
-                if ( tileChar == 'f' ) { game.addTile(new Position(i, j), GameConstants.FOREST); }
-                if ( tileChar == 'h' ) { game.addTile(new Position(i, j), GameConstants.HILLS); }			 
 			}
 
 			for ( int z = 0; z < unitLine.length(); z++){    
@@ -109,63 +131,65 @@ public class DeltaMapStrategy implements MapStrategy {
 				if ( unitChar == 'r' ) {
 					if ( unitLine.charAt(z + 1) == 'a' ) {
 						if (k < extra){
-							game.addUnit(new Position(i, (z-k)), GameConstants.ARCHER, Player.RED);
+							units[i][z-k] = new UnitImpl(GameConstants.ARCHER, Player.RED, new Position(i, z-k));
 							k++;
 						}
 					}             	            		
 				}
-				
+
 				/*if ( unitChar == 'b' ) {
-					if ( unitLine.charAt(z + 1) == 'a' ) {
-						if (k < extra){
-							game.addUnit(new Position(i, (z-k)), GameConstants.ARCHER, Player.BLUE);
-							k++;
-						}
-					}             	            		
-				}*/
-				
+							if ( unitLine.charAt(z + 1) == 'a' ) {
+								if (k < extra){
+									game.addUnit(new Position(i, (z-k)), GameConstants.ARCHER, Player.BLUE);
+									k++;
+								}
+							}             	            		
+						}*/
+
 				/** 
 				 * This is for red and blue LEGIONS
 				 */
 				/*if ( unitChar == 'r' ) {
-					if ( unitLine.charAt(z + 1) == 'l' ) {
-						if (k < extra){
-							game.addUnit(new Position(i, (z-k)), GameConstants.LEGION, Player.RED);
-							k++;
-						}
-					}             	            		
-				}*/
-				
+							if ( unitLine.charAt(z + 1) == 'l' ) {
+								if (k < extra){
+									game.addUnit(new Position(i, (z-k)), GameConstants.LEGION, Player.RED);
+									k++;
+								}
+							}             	            		
+						}*/
+
 				if ( unitChar == 'b' ) {
 					if ( unitLine.charAt(z + 1) == 'l' ) {
 						if (k < extra){
-							game.addUnit(new Position(i, (z-k)), GameConstants.LEGION, Player.BLUE);
+							units[i][z-k] = new UnitImpl(GameConstants.LEGION, Player.BLUE, new Position(i, z-k));
 							k++;
 						}
 					}             	            		
 				}
-				
+
 				/** 
 				 * This is for red and blue SETTLERS
 				 */
 				if ( unitChar == 'r' ) {
 					if ( unitLine.charAt(z + 1) == 's' ) {
 						if (k < extra){
-							game.addUnit(new Position(i, (z-k)), GameConstants.SETTLER, Player.RED);
+							units[i][z-k] = new UnitImpl(GameConstants.SETTLER, Player.RED, new Position(i, z-k));
 							k++;
 						}
 					}             	            		
 				}
 				/*
-				if ( unitChar == 'b' ) {
-					if ( unitLine.charAt(z + 1) == 's' ) {
-						if (k < extra){
-							game.addUnit(new Position(i, (z-k)), GameConstants.SETTLER, Player.BLUE);
-							k++;
-						}
-					}             	            		
-				}*/
+						if ( unitChar == 'b' ) {
+							if ( unitLine.charAt(z + 1) == 's' ) {
+								if (k < extra){
+									game.addUnit(new Position(i, (z-k)), GameConstants.SETTLER, Player.BLUE);
+									k++;
+								}
+							}             	            		
+						}*/
 			}
 		}
+		return units;
 	}
 }
+
